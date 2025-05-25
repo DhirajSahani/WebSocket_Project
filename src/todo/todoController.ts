e18c6f542a5adc91948bcde878a5e96a59c9ac9d
Dhiraj Sahani <dhirajonline2k20@gmail.com>
2025-05-25 22:45:52 +0545

    updatatedtodostatus

--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -1,70 +1,100 @@
 import { Socket } from "socket.io";
 import { getSocketIo } from "../../server";
 import todoModel from "./todoModel";
-import { ITodo, status } from "./todoTypes";
+import { ITodo, status as Status } from "./todoTypes";
 
 
 
-class Todo{
+class Todo {
     private io = getSocketIo();
-    constructor(){
-        this.io.on("connection",(socket:Socket)=>{
+    constructor() {
+        this.io.on("connection", (socket: Socket) => {
             console.log("new client connected!!")
-            socket.on("addTodo",(data)=>this.handleAddTodo(socket,data))
-            socket.on("deleteTodo",(data)=>this.handleDeleteTodo(socket,data))
+            socket.on("addTodo", (data) => this.handleAddTodo(socket, data))
+            socket.on("deleteTodo", (data) => this.handleDeleteTodo(socket, data))
+            socket.on("updateTodoStatus", (data) => this.handleUpdateTodoStatus(socket, data))
 
         })
 
     }
-private async handleAddTodo(socket: Socket, data:ITodo) {
-    try {
-        const { task, deadLine, status } = data;
-        await todoModel.create({
-            task,
-            deadLine,
-            status
-        });
-        const todos = await  todoModel.find()
+    private async handleAddTodo(socket: Socket, data: ITodo) {
+        try {
+            const { task, deadLine, status } = data;
+            await todoModel.create({
+                task,
+                deadLine,
+                status
+            });
+            const todos = await todoModel.find({status:Status.Pending })
 
-        socket.emit("todos_updated", {
-            status: "success",
-            data: todos
-        });
-    } catch (error) {
-        socket.emit("todo_response", {
-            status: "error",
-            message: "Failed to create todo",
-            error: error instanceof Error ? error.message : error
-        });
+            socket.emit("todos_updated", {
+                status: "success",
+                data: todos
+            });
+        } catch (error) {
+            socket.emit("todo_response", {
+                status: "error",
+                message: "Failed to create todo",
+                error: error instanceof Error ? error.message : error
+            });
+        }
     }
-}
 
-private async handleDeleteTodo(socket:Socket,data:{id:string}){
-try {
-        const {id}= data
-   const deletedTodo = await  todoModel.findByIdAndDelete(id)
-   if(!deletedTodo){
-    socket.emit("todo_response",{
-       status :"error",
-       message:"Todo not found"
-    })
-    return;
-   }
-     const todos = await  todoModel.find()
-        socket.emit("todos_updated", {
-            status: "success",
-            data: todos
-        });
-} catch (error) {
-       socket.emit("todo_response",{
-       status :"error",
-       error
-      
-    })
-}
+    private async handleDeleteTodo(socket: Socket, data: { id: string }) {
+        try {
+            const { id } = data
+            const deletedTodo = await todoModel.findByIdAndDelete(id)
+            if (!deletedTodo) {
+                socket.emit("todo_response", {
+                    status: "error",
+                    message: "Todo not found"
+                })
+                return;
+            }
+            const todos = await todoModel.find({ status:Status.Pending })
+            socket.emit("todos_updated", {
+                status: "success",
+                data: todos
+            });
+        } catch (error) {
+            socket.emit("todo_response", {
+                status: "error",
+                error
+
+            })
+        }
 
 
-}
+    }
+
+    private async handleUpdateTodoStatus(socket: Socket, data: { id: string, status: Status }) {
+        try {
+            const { id, status } = data
+            const todo = await todoModel.findByIdAndUpdate(id, {status})
+
+            if (!todo) {
+                socket.emit("todo_response", {
+                    status: "error",
+                    message: "todo not found"
+                })
+                return
+            }
+            const todos = await todoModel.find({ status:Status.Pending })
+            socket.emit("todos_updated", {
+                status: "success",
+                data: todos
+            });
+        } catch (error) {
+            socket.emit("todo_response", {
+                status: "error",
+                error
+
+            });
+
+        }
+
+
+    }
 
 }
 
